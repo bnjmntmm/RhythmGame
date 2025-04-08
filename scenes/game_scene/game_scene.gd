c41ea171f2b40bdc8d4ab396e9b266d1dc2c451f
timmno12 <benjamintimm21@gmail.com>
2025-04-08 22:42:53 +0200

    added a pause overlay

--- scenes/game_scene/game_scene.gd
+++ scenes/game_scene/game_scene.gd
@@ -1,5 +1,18 @@
 extends Node
 
+@onready var fade_overlay = %FadeOverlay
+@onready var pause_overlay = %PauseOverlay
+
 
 func _ready() -> void:
+	## only for transition and pause
+	fade_overlay.visible = true
+	#pause_overlay.game_exited.connect(_save_game) ## if we wanna have save stats
 	pass
+
+func _input(event) -> void:
+	if event.is_action_pressed("pause") and not pause_overlay.visible:
+		get_viewport().set_input_as_handled()
+		get_tree().paused = true
+		pause_overlay.grab_button_focus()
+		pause_overlay.visible = true
