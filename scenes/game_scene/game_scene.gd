40de590498f8fbc8d90be4143e960657f9b66b99
timmno12 <benjamintimm21@gmail.com>
2025-04-08 18:39:32 +0200

    Update game_scene.gd

--- scenes/game_scene/game_scene.gd
+++ scenes/game_scene/game_scene.gd
@@ -1,21 +1,4 @@
 extends Node
-@onready var sequenceGenerator: Sequence = $Sequence
-@onready var notes: Node2D = $UILAYER/UI/Notes
-@onready var spawn_location: Marker2D = $UILAYER/UI/SpawnLocation
-
-const NOTE_OBJECT = preload("res://scenes/NoteObject/note_object.tscn")
-
 
 func _ready() -> void:
-	sequenceGenerator.played_sound.connect(print_current_sound)
-	var sequence = sequenceGenerator.generate_random_sequence(4)
-	sequenceGenerator.play_sequence(sequence)
-	
-
-func print_current_sound(sound) -> void:
-	var new_note = NOTE_OBJECT.instantiate()
-	notes.add_child(new_note)
-	new_note.global_position = spawn_location.global_position
-	new_note.label.text = sound.name
-	new_note.allowed_to_move = true
-	
+	pass
