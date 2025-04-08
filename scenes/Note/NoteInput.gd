d2ee1ed2afcf20a1dfb1f633379a1a43078393a0
timmno12 <benjamintimm21@gmail.com>
2025-04-08 19:56:26 +0200

    Sound on Button Press
    - Added Sound on Button Press
    - Can be changed in the Sounds Dict
    - TODO: we need a better Sound (loops bad)

--- scenes/Note/NoteInput.gd
+++ scenes/Note/NoteInput.gd
@@ -7,11 +7,22 @@ signal released_note(note_name, duration)
 @export var inputName = ""
 var _time_when_pushed_down = 0
 var duration_pressed
+var is_pressed : bool = false
+var sound : AudioStream
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	%AudioStreamPlayer.stream = sound
+
+
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(inputName):
+		%AudioStreamPlayer.play()
+		is_pressed = true
 		_time_when_pushed_down = Time.get_ticks_msec()
+		
 		print("Pressed ", inputName)
 	if Input.is_action_just_released(inputName):
+		is_pressed = false
+		%AudioStreamPlayer.stop()
 		var duration = (Time.get_ticks_msec()- _time_when_pushed_down) / 1000.0
 		released_note.emit(inputName, duration)
--- scripts/input_controller.gd
+++ scripts/input_controller.gd
@@ -1,11 +1,15 @@
 extends Node
-@export var input_strings : Array[String]
+
+const NOTE = preload("res://scenes/Note/note.tscn")
 @export var short_press_duration : float = 0.12
+@export var input_strings : Array[String]
+@export var sounds : Dictionary[String, AudioStream]
 
 func _ready() -> void:
 	for input_string in input_strings:
-		var new_note = NoteInput.new()
+		var new_note = NOTE.instantiate()
 		new_note.inputName = input_string
+		new_note.sound = sounds[input_string]
 		new_note.released_note.connect(released_note)
 		add_child(new_note)
 
